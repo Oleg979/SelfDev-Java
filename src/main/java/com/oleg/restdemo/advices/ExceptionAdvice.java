d951ecd07fb45c01c54c5b4c69e715fced10daef
Олег <olegsolovev506@gmail.com>
2019-04-21 14:21:25 +0400

    initial commit

--- src/main/java/com/oleg/restdemo/advices/ExceptionAdvice.java
+++ src/main/java/com/oleg/restdemo/advices/ExceptionAdvice.java
@@ -5,15 +5,11 @@ import com.oleg.restdemo.exceptions.TaskNotFoundException;
 import com.oleg.restdemo.exceptions.UserAlreadyExistsException;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class ExceptionAdvice extends ResponseEntityExceptionHandler {
     @Data
     @AllArgsConstructor
@@ -27,27 +23,13 @@ public class ExceptionAdvice extends ResponseEntityExceptionHandler {
         private String result;
     }
 
-    @ResponseBody
     @ExceptionHandler(ResponseResultException.class)
-    public ResponseEntity<ResponseResult> responseResultHandler(ResponseResultException e) {
-        return new ResponseEntity<>(new ResponseResult("ok"), HttpStatus.OK);
+    public ResponseResult responseResultHandler(ResponseResultException e) {
+        return new ResponseResult("ok");
     }
 
-    @ResponseBody
-    @ExceptionHandler(TaskNotFoundException.class)
-    public ResponseEntity<ErrorResponse> TaskNotFoundHandler(TaskNotFoundException e) {
-        return new ResponseEntity<>(new ErrorResponse(e.getMessage()), HttpStatus.NOT_FOUND);
-    }
-
-    @ResponseBody
-    @ExceptionHandler(UserAlreadyExistsException.class)
-    public ResponseEntity<ErrorResponse> UserAlreadyExistsHandler(UserAlreadyExistsException e) {
-        return new ResponseEntity<>(new ErrorResponse(e.getMessage()), HttpStatus.BAD_REQUEST);
-    }
-
-    @ResponseBody
-    @ExceptionHandler(IllegalAccessException.class)
-    public ResponseEntity<ErrorResponse> IllegalAccessHandler(IllegalAccessException e) {
-        return new ResponseEntity<>(new ErrorResponse(e.getMessage()),  HttpStatus.BAD_REQUEST);
+    @ExceptionHandler({IllegalAccessException.class, TaskNotFoundException.class, UserAlreadyExistsException.class})
+    public ErrorResponse illegalAccessHandler(RuntimeException e) {
+        return new ErrorResponse(e.getMessage());
     }
 }
---
+++ src/main/java/com/oleg/restdemo/controllers/StatController.java
@@ -0,0 +1,34 @@
+package com.oleg.restdemo.controllers;
+
+import com.oleg.restdemo.models.Task;
+import com.oleg.restdemo.repos.TaskRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@RestController
+@Slf4j
+@RequestMapping("stats")
+public class StatController {
+    private final TaskRepository taskRepository;
+
+    @Autowired
+    public StatController(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    @GetMapping
+    public Map<String, List<Task>> getStats(@AuthenticationPrincipal String name) {
+        return taskRepository
+                .findAllByUserName(name)
+                .stream()
+                .collect(Collectors.groupingBy(t -> t.getCreationDate().toString()));
+    }
+}
