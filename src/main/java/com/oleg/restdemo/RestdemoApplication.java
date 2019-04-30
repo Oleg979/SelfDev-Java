52570982b0d6ba1c36b26ad3772a81831bfa017e
Олег <olegsolovev506@gmail.com>
2019-04-30 15:03:23 +0400

    commit

--- src/main/java/com/oleg/restdemo/RestdemoApplication.java
+++ src/main/java/com/oleg/restdemo/RestdemoApplication.java
@@ -6,6 +6,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import com.zaxxer.hikari.*;
+import org.springframework.beans.factory.annotation.Value;
+import javax.sql.DataSource;
 import java.util.TimeZone;
 
 @SpringBootApplication
