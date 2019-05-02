654243ff18a7010c3486b3f6ed1d0eeee8f592e7
Олег <olegsolovev506@gmail.com>
2019-05-02 11:31:12 +0400

    commit cors

--- src/main/java/com/oleg/restdemo/controllers/StatController.java
+++ src/main/java/com/oleg/restdemo/controllers/StatController.java
@@ -5,6 +5,7 @@ import com.oleg.restdemo.repos.TaskRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
